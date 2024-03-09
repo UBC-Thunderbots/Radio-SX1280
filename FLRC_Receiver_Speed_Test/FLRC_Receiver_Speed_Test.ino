f829289f4a2e0bee2b93777de93f5004cfd026fe
matthewtng <matthew0517@hotmail.com>
2024-03-09 12:37:27 -0800

    Packet Variable Length -> Fixed Variable Length

--- FLRC_Receiver_Speed_Test/FLRC_Receiver_Speed_Test.ino
+++ FLRC_Receiver_Speed_Test/FLRC_Receiver_Speed_Test.ino
@@ -255,7 +255,7 @@ void setup()
   LT.setRfFrequency(Frequency, Offset);
   LT.setBufferBaseAddress(0, 0);
   LT.setModulationParams(BandwidthBitRate, CodingRate, BT);
-  LT.setPacketParams(PREAMBLE_LENGTH_08_BITS, FLRC_SYNC_WORD_LEN_P32S, RADIO_RX_MATCH_SYNCWORD_1, RADIO_PACKET_VARIABLE_LENGTH, RXBUFFER_SIZE, RADIO_CRC_OFF, RADIO_WHITENING_OFF);
+  LT.setPacketParams(PREAMBLE_LENGTH_08_BITS, FLRC_SYNC_WORD_LEN_P32S, RADIO_RX_MATCH_SYNCWORD_1, RADIO_PACKET_FIXED_LENGTH, RXBUFFER_SIZE, RADIO_CRC_OFF, RADIO_WHITENING_OFF);
   LT.setDioIrqParams(IRQ_RADIO_ALL, (IRQ_TX_DONE + IRQ_RX_TX_TIMEOUT), 0, 0);              //set for IRQ on TX done and timeout on DIO1
   LT.setSyncWord1(Syncword);
   LT.setSyncWordErrorTolerance(2);
